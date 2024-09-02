372463ef0e4e098f8d6918bc62bb7536ad039bb5
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-02 11:22:04 -0300

    Manual tests of AgendaContatos and a method toString to improve the visualization

--- src/AgendaContatos.java
+++ src/AgendaContatos.java
@@ -56,4 +56,50 @@ public class AgendaContatos {
         return null;
     }
 
+    /**
+     * Return a string representation of one object of this class based on its values
+     *
+     * @return A string representation of one object of this class based on its values
+     */
+    public String toString() {
+        String ret = "{";
+        for (Map.Entry<String, Integer> item : agendaContatoMap.entrySet()) {
+            ret = ret + item.getKey() + ":" + item.getValue() + ", ";
+        }
+        // Removing the last comma and space
+        if (agendaContatoMap.size() != 0) {
+            ret = ret.substring(0, ret.length() - 2);
+        }
+        ret = ret + "}";
+        return ret;
+    }
+
+    /**
+     * Main method to manual tests
+     * 
+     * @param args Arguments passed through CLI (ignored)
+     */
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        System.out.println(agendaContatos);
+
+        agendaContatos.adicionarContato("André", 1);
+        agendaContatos.adicionarContato("Júlio", 2);
+        agendaContatos.adicionarContato("Fátima", 3);
+        agendaContatos.adicionarContato("Thais", 0);
+        // The method adicionarContato internally uses the put method of a hashmap
+        // This method updates a value based on the key passed instead of ignoring
+        agendaContatos.adicionarContato("Thais", 4);
+        System.out.println(agendaContatos);
+
+        // See in the output that the map mantain the relation with the key that was added, but is unordered
+
+        System.out.println(agendaContatos.pesquisarPorNome("Fátima"));
+
+        agendaContatos.removerContato("Thais");
+        System.out.println(agendaContatos);
+
+        agendaContatos.exibirContatos();
+    }
+
 }
